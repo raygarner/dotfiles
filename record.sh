b26c5b8257ba28e8b2fe1b65d3f3dfdedd5d3395
Ray Garner <ray@perfectcast.co.uk>
2020-03-03 16:57:44 +0000

    removed external input recording from record.sh

--- record.sh
+++ record.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #ffmpeg -f x11grab -r 24 -s 1920x1080 -i :0.0 -f pulse -i 7 $1
 
-ffmpeg -f x11grab -r 24 -s 1920x1080 -i :0.0 -f pulse -ac 1 -i 8 -f pulse -ac 2 -i 7 -map 0 -map 1 -map 2 -filter_complex amerge=inputs=2 $1
+ffmpeg -f x11grab -r 24 -s 1920x1080 -i :0.0 -f pulse -i default $1
 #-i :0.0 sets the screen id to record from
